17f9ab2735e659c8c076f10f6c2af4eb7a77a3be
Seán Ó Fithcheallaigh <sofithcheallaigh@gmail.com>
2023-01-29 13:10:41 +0000

    commented some code out to run a simple task.

--- MATLAB/classification_examples/object_test_run/data_builder.m
+++ MATLAB/classification_examples/object_test_run/data_builder.m
@@ -14,7 +14,12 @@ splitPercentage = 0.70;
 % m1 is the number of the training data
 m1 = round(splitPercentage*rows);
 trainingData = T(idx(1:m1),:);
-test_data = T(idx(m1+1:end),:);
+testData = T(idx(m1+1:end),:);
+
+filename1 = "trainingData.csv";
+filename2 = "testData.csv";
+writetable(trainingData,filename1);
+writetable(testData,filename2);
 
 predictor_names = {'Channel1', 'Channel2'};
 
@@ -24,7 +29,7 @@ response = trainingData.LabelObject;
 toNormalise = T(:,predictor_names);
 N = normalize(toNormalise,'range');
 
-mdl = fitcknn(N,T.LabelObject,'NumNeighbors',3);
+mdl = fitcknn(N,T.LabelObject,'NumNeighbors',2);
 cvmdl1 = crossval(mdl,'KFold',5);
 cvmdl2 = crossval(mdl,'Holdout',0.3);
 
