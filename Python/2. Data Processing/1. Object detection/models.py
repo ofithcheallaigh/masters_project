ae1121629deefefadf215f5004b97d9fb85b8448
Seán Ó Fithcheallaigh <sofithcheallaigh@gmail.com>
2023-02-06 23:17:06 +0000

    updating model to try and process data for grid analysis

--- Python/2. Data Processing/1. Object detection/models.py
+++ Python/2. Data Processing/1. Object detection/models.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import seaborn as sns
 import numpy as np
+from statistics import mean 
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
@@ -24,14 +25,43 @@ from my_fns import modify_to_grid_zero_fn
 dataset1 = pd.read_csv("Python\\2. Data Processing\\0. Data\midhallway_displaystand_final_dataset.csv")
 dataset2 = pd.read_csv("Python\\2. Data Processing\\0. Data\midhallway_clear_final_dataset.csv")
 
+
+len_dataset1 = len(dataset1)
+len_dataset2 = len(dataset2)
+
+grid_dataset1_lengths = []
+grid_dataset2_lengths = []
+# for y in range(len_dataset1):
+for x in range(1,10):
+    grid_length1 = len(dataset1[dataset1['Grid']==x])
+    grid_dataset1_lengths.append(grid_length1)
+    grid_length2 = len(dataset1[dataset2['Grid']==x])
+    grid_dataset2_lengths.append(grid_length2)
+
+## Balancing dataset
+# Now I have the lengths for the data in each grid, I need to start building a new dataset for the grid 0 data
+data_points = sum(grid_dataset2_lengths) / len(grid_dataset2_lengths)
+data_points = data_points / len(grid_dataset2_lengths) # I will need to take this value from each grid data
+data_points = int(data_points)
+
+# Now to try and build the dataset
+# new_dataset = pd.DataFrame(columns=["Channel1", "Channel2", "LabelObject", "Grid"])
+new_dataset = pd.DataFrame()
+for x in range(1,10):
+    temp_data = dataset2[dataset2['Grid']==x]
+    temp_data = temp_data.iloc[:data_points,:]
+    new_dataset = new_dataset.append(temp_data)
+
+
+# new_dataset.to_csv("foofoo.csv")
 # pf.modify_to_grid_zero_fn(dataset2)
-dataset2 = modify_to_grid_zero_fn(dataset2)
+dataset2 = modify_to_grid_zero_fn(new_dataset)
 
 data = np.vstack((dataset1,dataset2)) # Create a variable with one dataset stacked on the other
 
 data = pd.DataFrame(data) # Convert to DataFrame
 data.columns=["Channel1","Channel2","LabelObject","Grid"] # Assign column names
-# data.to_csv('out.txt') # This was here to output a text file to show Ian
+data.to_csv('out.csv') # This was here to output a text file to show Ian
 
 # pick out rows depending on grid num
 # This was added in to just pick out grid pos 1
@@ -39,14 +69,16 @@ data.columns=["Channel1","Channel2","LabelObject","Grid"] # Assign column names
 # data = grid1_data
 
 feature_names = ['Channel1','Channel2'] # Features we are interested in
-response = data['LabelObject'] # Response we are interested in
+response = data['Grid'] # Response we are interested in
+# response = data['LabelObject'] # Response we are interested in
 
 # Replaces Yes/No with 1/0. Required because some algo will not accept categorical data
 data.LabelObject.replace(('Yes', 'No'), (1, 0), inplace=True) 
 
 # X and y are what will be passed through the algorithms to train the model
 X = data[feature_names]
-y = data['LabelObject']
+# y = data['LabelObject']
+y = data['Grid']
 
 
 # cmap = cm.get_cmap('gnuplot')
@@ -80,18 +112,29 @@ plt.show()
 plt.savefig('scatter_matrix_object_display_stand.jpg') 
 """
 
-# The sections below generate the model accuracy scores
+""" # The sections below generate the model accuracy scores
 # Model: Logistic regression
 logReg = LogisticRegression()
 logReg.fit(X_train, y_train)
 print('Accuracy of Logistic regression classifier on training set: {:.2f}'.format(logReg.score(X_train, y_train))) # 0.75
-print('Accuracy of Logistic regression classifier on test set: {:.2f}'.format(logReg.score(X_test, y_test))) # 0.47
+print('Accuracy of Logistic regression classifier on test set: {:.2f}'.format(logReg.score(X_test, y_test))) # 0.47 """
+
+
+
+
 
 # Model: Decision tree
 clf = DecisionTreeClassifier().fit(X_train, y_train)
 print('Accuracy of Decision Tree classifier on training set: {:.2f}'.format(clf.score(X_train, y_train))) # 1.00
 print('Accuracy of Decision Tree classifier on test set: {:.2f}'.format(clf.score(X_test, y_test))) # 0.87
 
+
+
+
+
+
+
+"""
 # Model: KNN
 knn = KNeighborsClassifier()
 knn.fit(X_train,y_train)
@@ -110,8 +153,9 @@ gnb.fit(X_train, y_train)
 print('Accuracy of GNB classifier on training set: {:.2f}'.format(gnb.score(X_train, y_train))) # 0.86
 print('Accuracy of GNB classifier on test set: {:.2f}'.format(gnb.score(X_test, y_test))) # 0.67  
 
-""" # Model: SVM
+# Model: SVM
 svm = SVC()
 svm.fit(X_train, y_train)
 print('Accuracy of SVM classifier on training set: {:.2f}'.format(svm.score(X_train, y_train))) # 0.91
-print('Accuracy of SVM classifier on test set: {:.2f}'.format(svm.score(X_test, y_test))) # 0.80 """
+print('Accuracy of SVM classifier on test set: {:.2f}'.format(svm.score(X_test, y_test))) # 0.80 
+"""
