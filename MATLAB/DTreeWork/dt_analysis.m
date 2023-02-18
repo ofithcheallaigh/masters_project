9011462bc7b440f5abd3fa742113418243c7ed08
Seán Ó Fithcheallaigh <sofithcheallaigh@gmail.com>
2023-02-18 22:51:12 +0000

    modified file for scatter plotting, later created new file for this

--- MATLAB/DTreeWork/dt_analysis.m
+++ MATLAB/DTreeWork/dt_analysis.m
@@ -3,6 +3,9 @@
 % 
 % inputTable = vertcat(closed,open); % Brings arrays together
 
+% Setting up colours for plotting
+colourArray = ["#F73309", "#27F10E", "#EBF10E", "#EB0EF1", "#0E14F1", "#A4A5BC", "#ADF5CA", "#8B0E31", "#43F9FD"];
+
 data_folder = "D:\Courses\UUJ\Research Project\masters_project\MATLAB\Data";
 run_folder = pwd;
 
@@ -12,6 +15,13 @@ cd(data_folder);
 input_table = readtable(strcat(path,file));
 
 inputTable = input_table;
+
+% Scatter plotting
+for i = 1:9
+    plotTable = inputTable(inputTable.Grid == i, :);
+    scatter(plotTable.Channel1, plotTable.Channel2,'color',colourArray{i},'marker','o')
+    hold on
+end
 
 % Change the Pclass from number to char (categorical variable) for
 % convience 
