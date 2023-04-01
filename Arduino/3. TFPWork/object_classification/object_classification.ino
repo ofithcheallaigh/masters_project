a0561d370c386200d19139a0c32ab5562f61a6c5
Seán Ó Fithcheallaigh <sofithcheallaigh@gmail.com>
2023-04-01 23:08:39 +0100

    adding object classification code

--- Arduino/3. TFPWork/object_classification/object_classification.ino
+++ Arduino/3. TFPWork/object_classification/object_classification.ino
@@ -1,3 +1,43 @@
+/*
+This code is used to run the generated model from my object detection project
+*/
+
+#include <TensorFlowLite.h>
+#include <tensorflow/lite/micro/all_ops_resolver.h>
+#include <tensorflow/lite/micro/tflite_bridge/micro_error_reporter.h> // Had to add "tflite_bridge"
+#include <tensorflow/lite/micro/micro_interpreter.h>
+#include <tensorflow/lite/schema/schema_generated.h>
+#include <tensorflow/lite/version.h>
+
+#include "object_model_50_128_sdg.h"
+
+const int numSamples = 10000;
+int samplesRead = numSamples;
+
+// Global variable used for TF Lite Micro
+tflite::MicroErrorReporter tflErrorReporter;
+
+// Import all the TF Lite Micro ops. I could pull in just the ones I need.
+// For the minute I will take everything in. But this may change.
+tflite::AllOpsResolver tflOpsResolver;
+
+// Used to declare a variable that will be used to store a reference to a TF Lite model
+const tflite::Model* tflModel = nullptr;
+
+tflite::MicroInterpreter* tflInterpreter = nullptr;
+TfLiteTensor* tflInputTensor = nullptr;
+TfLiteTensor* tflOutputTensor = nullptr;
+
+// Create a memory buffer in the micro:
+// byte define the type of the array and `tensorArena` is the name of the array.
+// [tensorArenaSize] is the size of the array. In this case, the array has a size of tensorArenaSize bytes.
+// __attribute__((aligned(16))) is an attribute that tells the compiler to align the memory for 
+// the array on a 16-byte boundary.
+constexpr int tensorArenaSize = 8 * 1024;
+byte tensorArena[tensorArenaSize] __attribute__((aligned(16)));
+
+// Array to map grids
+
 void setup() {
   // put your setup code here, to run once:
 
